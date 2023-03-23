b4e2ee3752642e7f5a681fe6e4e18de08ceaf9a0
kwiatkowski-michal <kwiatkowskimichal04@gmail.com>
2023-03-23 10:29:57 +0100

    Some file cleaining, google maps embed and new container

--- kruszyn-krajenski/smieci.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    let frameElement = document.getElementById("myiFrame");
-    let doc = frameElement.contentDocument;
-    doc.body.innerHTML = doc.body.innerHTML + '<style>.bar {width:45%;}</style>';
-  }
